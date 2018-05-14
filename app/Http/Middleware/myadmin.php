94bc96ff0f68f461d5ab8f20423fc6e868aa7148
newworkakountmaxim <newworkakount@gmail.com>
2018-05-14 18:10:03 +0300

    Backpack v2 START

---
+++ app/Http/Middleware/myadmin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use Illuminate\Support\Facades\Auth;
+
+class myadmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // проверяем принадлежность пользователя
+        //if ( Auth::check() && Auth::user()->isAdmin()==true )
+        if ( Auth::check() )
+        {
+            //return $next($request);
+            return 1;
+        }
+        return redirect('/');
+    }
+}
